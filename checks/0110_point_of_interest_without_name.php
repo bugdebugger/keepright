0251a97e33d085ccd6316121e739d63fc6aa1e19
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2016-12-11 15:09:11 +0000

    contributed by Jan: fast_food and fuel need name or operator or brand; remove unneeded views

--- checks/0110_point_of_interest_without_name.php
+++ checks/0110_point_of_interest_without_name.php
@@ -8,39 +8,42 @@ NAME required for
 place_of_worship, pub, restaurant, school, university, hospital, library, theatre, courthouse
 
 NAME or OPERATOR required for
-bank, fast_food, cinema, pharmacy, cafe, fuel
+bank, cinema, pharmacy, cafe
+
+NAME or OPERATOR or BRAND required for
+fast_food, fuel
+
 */
 
 
-query("
-	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
-	SELECT node_id, MIN(v) as v
-	FROM node_tags
-	WHERE k='amenity' AND v IN ('place_of_worship', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse')
-	GROUP BY node_id
-", $db1);
+
 query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, txt1, last_checked)
-	SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name tag', htmlspecialchars(v), NOW()
-	FROM _tmp_points_of_interest b
-	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
-", $db1);
-
-
-query("
-	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
-	SELECT node_id, MIN(v) as v
-	FROM node_tags
-	WHERE k='amenity' AND v IN ('bank', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'fuel')
+	SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name tag', htmlspecialchars(MIN(v)), NOW()
+	FROM node_tags b
+	WHERE k='amenity' AND v IN ('place_of_worship', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse') AND
+		NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
 	GROUP BY node_id
 ", $db1);
+
+
 query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, txt1, last_checked)
-	SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name tag or an operator tag', htmlspecialchars(v), NOW()
-	FROM _tmp_points_of_interest b
-	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator'))
+	SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name tag or an operator tag', htmlspecialchars(MIN(v)), NOW()
+	FROM node_tags b
+	WHERE k='amenity' AND v IN ('bank', 'cinema', 'pharmacy', 'cafe') AND
+		NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator') )
+	GROUP BY node_id
 ", $db1);
 
 
-query("DROP VIEW _tmp_points_of_interest", $db1);
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, txt1, last_checked)
+	SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name tag or an operator tag or a brand tag', htmlspecialchars(MIN(v)), NOW()
+	FROM node_tags b
+	WHERE k='amenity' AND v IN ('fast_food', 'fuel') AND
+		NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator', 'brand') )
+	GROUP BY node_id
+", $db1);
+
 ?>
